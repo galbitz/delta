2a5fd0c2518a48f5defe9f205c50ede7c607f7b1
Gabor A <galbitz@gmail.com>
2019-01-03 16:55:40 -0500

    Added load balancers and configurable worker nodes (#1)

--- env/k8master.tf
+++ env/k8master.tf
@@ -1,33 +1,57 @@
-
-resource "azurerm_public_ip" "master-ip" {
-  name                         = "${var.namespace}-master-ip"
-  location                     = "${azurerm_resource_group.rg.location}"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
-  public_ip_address_allocation = "Dynamic"
-  idle_timeout_in_minutes      = 30
-}
-
-resource "azurerm_network_interface" "master-nic" {
-  name                = "${var.namespace}-master-nic"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-
-  ip_configuration {
-    name                          = "master-ip-config"
-    subnet_id                     = "${azurerm_subnet.subnet.id}"
-    private_ip_address_allocation = "static"
-    private_ip_address            = "10.0.0.5"
-    public_ip_address_id          = "${azurerm_public_ip.master-ip.id}"
+variable "master-ips" {
+  default = {
+    "0" = "10.0.0.5"
+    "1" = "10.0.0.6"
   }
 }
 
-resource "azurerm_virtual_machine" "master-vm" {
-  name                         = "${var.namespace}-master-vm"
+variable "master-count" {
+  default = 1
+}
+
+resource "azurerm_availability_set" "master-avset" {
+  name                         = "${var.namespace}-master-avset"
   location                     = "${azurerm_resource_group.rg.location}"
   resource_group_name          = "${azurerm_resource_group.rg.name}"
-  network_interface_ids        = ["${azurerm_network_interface.master-nic.id}"]
-  primary_network_interface_id = "${azurerm_network_interface.master-nic.id}"
+  managed                      = true
+}
+
+resource "azurerm_network_interface" "master-nic" {
+  name                = "${var.namespace}-master-nic-${count.index}"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  count               = "${var.master-count}"
+
+  ip_configuration {
+    name                          = "master-ip-config${count.index}"
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    private_ip_address_allocation = "static"
+    private_ip_address            = "${lookup(var.master-ips,count.index)}"
+  }
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "master-addresspool" {
+  network_interface_id    = "${element(azurerm_network_interface.master-nic.*.id,count.index)}"
+  ip_configuration_name   = "master-ip-config${count.index}"
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.backend_pool.id}"
+  count                   = "${var.master-count}"
+}
+
+resource "azurerm_network_interface_nat_rule_association" "master-nat" {
+  network_interface_id  =  "${element(azurerm_network_interface.master-nic.*.id,count.index)}"
+  ip_configuration_name = "master-ip-config${count.index}"
+  nat_rule_id           = "${element(azurerm_lb_nat_rule.ssh_rule_master.*.id,count.index)}"
+  count                 = "${var.master-count}"
+}
+
+resource "azurerm_virtual_machine" "master-vm" {
+  name                         = "${var.namespace}-master-vm-${count.index}"
+  location                     = "${azurerm_resource_group.rg.location}"
+  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  availability_set_id          = "${azurerm_availability_set.master-avset.id}"
+  network_interface_ids        = ["${element(azurerm_network_interface.master-nic.*.id,count.index)}"]
   vm_size                      = "${var.vmsize}"
+  count                        = "${var.master-count}"
 
   delete_os_disk_on_termination = true
 
@@ -41,13 +65,13 @@ resource "azurerm_virtual_machine" "master-vm" {
   }
 
   storage_os_disk {
-    name          = "${var.namespace}-master-disk1"
+    name          = "${var.namespace}-master-disk-${count.index}"
     caching       = "ReadWrite"
     create_option = "FromImage"
   }
 
   os_profile {
-    computer_name  = "k8master"
+    computer_name  = "k8master${count.index}"
     admin_username = "${var.adminusername}"
     admin_password = "${var.adminpassword}"
   }
@@ -61,7 +85,7 @@ resource "azurerm_virtual_machine_extension" "master-vm-extension" {
   name                 = "linuxext"
   location             = "${azurerm_resource_group.rg.location}"
   resource_group_name  = "${azurerm_resource_group.rg.name}"
-  virtual_machine_name = "${azurerm_virtual_machine.master-vm.name}"
+  virtual_machine_name = "${element(azurerm_virtual_machine.master-vm.*.name,0)}"
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
@@ -76,14 +100,4 @@ resource "azurerm_virtual_machine_extension" "master-vm-extension" {
 
     }
 SETTINGS
-}
-
-data "azurerm_public_ip" "master-public-ip" {
-  name                = "${azurerm_public_ip.master-ip.name}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  depends_on          = ["azurerm_virtual_machine.master-vm"]
-}
-
-output "master-vm-ip_address" {
-  value = "${data.azurerm_public_ip.master-public-ip.ip_address}"
 }
